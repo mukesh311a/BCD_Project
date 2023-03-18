27a71244c5def791ebc7a042e1706731856306bb
mukesh311a <mukesh.gbu@gmail.com>
2023-03-18 13:32:51 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php 
-$conn = mysqli_connect('localhost','weislabc_bcd','bcd@123','weislabc_bcd');
+$conn = mysqli_connect('localhost','weislabc_bcd.sql','bcd@123','weislabc_bcd');
 if (mysqli_connect_errno()) {
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	exit();
